a50c563142b6d0f67a0b78fe04f35b57feb32ffd
joshdboss <newbie856@gmail.com>
2022-01-04 09:20:10 -0500

    started routing

--- data_logger/src/main.cpp
+++ data_logger/src/main.cpp
@@ -36,15 +36,15 @@ const bool DEBUG = false;
 
 // Pins ------------------------------------------------------------------------
 // I/O
-const uint8_t GREEN_LED_PIN = 7,
-              RED_LED_PIN = 8,
-              BUTTON0_PIN = 2,
-              BUTTON1_PIN = 6;
+const uint8_t GREEN_LED_PIN = 23,
+              RED_LED_PIN = 22,
+              BUTTON0_PIN = 21,
+              BUTTON1_PIN = 20;
 // ADIS164760
 const uint8_t DR_ADIS16470_PIN = 24,
               SYNC_ADIS16470_PIN = 25,
               RST_ADIS16470_PIN = 35,
-              CS_ADIS16470_PIN = 36;
+              CS_ADIS16470_PIN = 2;
 // AIS1120SX
 const uint8_t CS_AIS1120SX_PIN = 31;
 // Pressure sensors
@@ -59,7 +59,7 @@ const uint8_t CS_RSC_ADC_PIN[2] = {16, 27};
 const uint8_t DR_RSC[2] = {15, 26};
 // Thermocouples
 const uint8_t TAT_TC = 3;
-const uint8_t CS_TCS_PIN[4] = {23, 22, 21, 20};
+const uint8_t CS_TCS_PIN[4] = {7, 8, 9, 10};
 // Altimax
 const uint8_t ALTIMAX_DR_PIN = 33;
 
