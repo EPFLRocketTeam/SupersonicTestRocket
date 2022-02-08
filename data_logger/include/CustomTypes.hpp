bd6dd7e004bb02095eb68ba622ea021a75dd02fc
spacewolfXfr <maelfeurgard@gmail.com>
2022-02-08 10:41:24 +0100

    Documented CustomTypes.hpp

--- data_logger/include/CustomTypes.hpp
+++ data_logger/include/CustomTypes.hpp
@@ -6,30 +6,36 @@
  * @brief Define how measurements can be due
  * 
  */
-
 typedef enum
 {
-    NOT_DUE,     // measurement not due
-    DUE_BY_TIME, // due by time
-    DUE_BY_DR    // due by DR input
+    NOT_DUE,     ///< Measurement not due
+    DUE_BY_TIME, ///< Due by time
+    DUE_BY_DR    ///< Due by Data Ready input
 } dueType;
 
-// define packet type numbering
+/**
+ * @brief Define different types of packets; use 1 byte
+ */
 enum packetType : uint8_t
 {
-    NO_PACKET,                     // no packet. indicates EOF
-    ADIS16470_PACKET_ARCHIVE_TYPE, // ADIS16470 Archived Packet
-    AISx120SX_PACKET_ARCHIVE_TYPE, // AISx120SX Archived Packet
-    RSC_PRESSURE_PACKET_TYPE,      // Honewell RSC pressure packet
-    RSC_TEMP_PACKET_TYPE,          // Honewell RSC temperature packet
-    MAX31855_PACKET_ARCHIVE_TYPE,  // MAX31855 Archived packet
-    ALTIMAX_PACKET_TYPE,           // ALTIMAX packet
-    ADIS16470_PACKET_TYPE,         // ADIS16470 Packet
-    AISx120SX_PACKET_TYPE,         // AISx120SX Packet
-    MAX31855_PACKET_TYPE           // MAX31855 packet
+    NO_PACKET,                     ///< no packet. indicates EOF
+    ADIS16470_PACKET_ARCHIVE_TYPE, ///< ADIS16470 Archived Packet
+    AISx120SX_PACKET_ARCHIVE_TYPE, ///< AISx120SX Archived Packet
+    RSC_PRESSURE_PACKET_TYPE,      ///< Honewell RSC pressure packet
+    RSC_TEMP_PACKET_TYPE,          ///< Honewell RSC temperature packet
+    MAX31855_PACKET_ARCHIVE_TYPE,  ///< MAX31855 Archived packet
+    ALTIMAX_PACKET_TYPE,           ///< ALTIMAX packet
+    ADIS16470_PACKET_TYPE,         ///< ADIS16470 Packet
+    AISx120SX_PACKET_TYPE,         ///< AISx120SX Packet
+    MAX31855_PACKET_TYPE           ///< MAX31855 packet
 };
-//typedef uint8_t packetType;
 
+/**
+ * @brief Convert from \p packetType to a printable string 
+ * 
+ * @param t : The packet
+ * @return const char* : Its string representation
+ */
 const char *packetTypeStr(const packetType t)
 {
     switch (t)
