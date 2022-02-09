34351582806460cb3efecdfc52c4115fce58e408
spacewolfXfr <maelfeurgard@gmail.com>
2022-02-09 18:18:03 +0100

    Shortened names

--- data_logger/include/CustomTypes.hpp
+++ data_logger/include/CustomTypes.hpp
@@ -46,39 +46,39 @@ const char *packetTypeStr(const packetType t)
         break;
 
     case ADIS16470_PACKET_ARCHIVE_TYPE:
-        return "ADIS16470_PACKET_ARCHIVE_TYPE";
+        return "ADIS16470_ARCHIVE";
         break;
 
     case AISx120SX_PACKET_ARCHIVE_TYPE:
-        return "AISx120SX_PACKET_ARCHIVE_TYPE";
+        return "AISx120SX_ARCHIVE";
         break;
 
     case RSC_PRESSURE_PACKET_TYPE:
-        return "RSC_PRESSURE_PACKET_TYPE";
+        return "RSC_PRESSURE";
         break;
 
     case RSC_TEMP_PACKET_TYPE:
-        return "RSC_TEMP_PACKET_TYPE";
+        return "RSC_TEMP";
         break;
 
     case MAX31855_PACKET_ARCHIVE_TYPE:
-        return "MAX31855_PACKET_ARCHIVE_TYPE";
+        return "MAX31855_ARCHIVE";
         break;
 
     case ALTIMAX_PACKET_TYPE:
-        return "ALTIMAX_PACKET_TYPE";
+        return "ALTIMAX";
         break;
 
     case ADIS16470_PACKET_TYPE:
-        return "ADIS16470_PACKET_TYPE";
+        return "ADIS16470";
         break;
 
     case AISx120SX_PACKET_TYPE:
-        return "AISx120SX_PACKET_TYPE";
+        return "AISx120SX";
         break;
 
     case MAX31855_PACKET_TYPE:
-        return "MAX31855_PACKET_TYPE";
+        return "MAX31855";
         break;
 
     default:
